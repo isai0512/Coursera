e7dd1a2a4c251d6953dedd50698188a17816fee7
Ricardo Martinez <isai_oxt@hotmail.com>
2016-08-11 17:16:40 -0500

    Initial Commit

---
+++ github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by Ricardo Isai on 11/08/16.
+//  Copyright © 2016 Ricardo Isai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by Ricardo Isai on 11/08/16.
+//  Copyright © 2016 Ricardo Isai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
